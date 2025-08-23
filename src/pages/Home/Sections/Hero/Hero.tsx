f8a84dd1675010b5c1873ad6b761da9309cf996a
federico-marini <federico97marini@gmail.com>
2025-08-23 11:58:56 +0200

    Update Hero.tsx

--- src/pages/Home/Sections/Hero/Hero.tsx
+++ src/pages/Home/Sections/Hero/Hero.tsx
@@ -11,12 +11,7 @@ export const Hero = () => {
       <p className={styles.subtitle}>{t("heroSectionDescription")}</p>
       /*
       <div className="flex justify-center">
-        <button
-          onClick={() => (window.location.href = CHAT_URL)}
-          className="cursor-pointer bg-gradient-to-r mt-8 from-green-500 to-lime-400 text-white px-6 py-2 rounded-md font-semibold text-xl shadow hover:-translate-y-1 transition-transform flex items-center gap-2 disabled:opacity-60 disabled:cursor-not-allowed mt-4"
-        >
-          {t("testDemo")}
-        </button>
+
       </div>
       */
     </section>
